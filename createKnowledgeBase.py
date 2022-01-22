e3cd4d4995f934c08e661aafebf309cb56413009
Tsaros <kgewrgitsaros@gmail.com>
2022-01-22 14:14:00 +0200

    αναπτηγμα του resolution

--- createKnowledgeBase.py
+++ createKnowledgeBase.py
@@ -12,24 +12,25 @@ def returnRandomCharacters(p):
         retString += tmpChar
     return retString
 
-def retSentance(l, p):
+def retSentance(l, p,availableCharacters):
     """ Επιστρέφει μια πρόταση τυχαίου μηκους (απο 0 εως l) με p πληθος διαθέσημων λεκτικών"""
 
     l = random.randint(1, l)  # Οι προτάσεις μπορει να έχουν μήκος απο 1 εως l (πλήθος λεκτικων)
-    # Ρωτα αν γινεται το l να ειναι 0
     sentance = ''
-    availableCharacters = returnRandomCharacters(p)
     # τα λεκτικά της πρότασης επιλέγονται απο το string availableCharacters εχει len()=p
     for i in range(l):  # επιλέγουμε l λεκτικά (μέγεθος πρότασης)
-        tmpChar = ''
-        while tmpChar in sentance:  # αν το λεκτικό που επιλεχθηκε ήδη υπάρχει επιλέγουμε αλλο
-            tmpChar = availableCharacters[random.randint(0, p-1)]
+        tmpChar = availableCharacters[random.randint(0, p - 1)]
+        flipChance = random.randint(0, 1)
+        if (flipChance == 1):
+            tmpChar = tmpChar.swapcase()  # Αναστρέφουμε τον χαρακτήρα
+        while (tmpChar in sentance or tmpChar.swapcase() in sentance):  # αν το λεκτικό που επιλεχθηκε ήδη υπάρχει επιλέγουμε αλλο
+            tmpChar = availableCharacters[random.randint(0, p - 1)]
             flipChance = random.randint(0, 1)
             if (flipChance == 1):
                 tmpChar = tmpChar.swapcase()  # Αναστρέφουμε τον χαρακτήρα
-            # TODO: ελεγχο για το αν ο not(γυρισμένος) ειναι ήδη στην πρώταση
-
         sentance += tmpChar  # κάθε νεο επιλεγμένο προσθέτεται στην πρόταση
+
+
     return sentance
 
 
@@ -38,15 +39,22 @@ def readKnowledgeBase():
         returns:(array) πινακας με πρωτη γραμμη τα χαρακτηριστικα (c,l,p)
                 οι υπολυπες γραμμες οι προτάσεις """
 
+    c,l,p=retKBcharacteristics()
+    i=0
     kb = []
     kbfile = open('knowledgeBase.txt', 'r')
     for line in kbfile:
-        if (line[0].isnumeric()):
+        if (i==0 or i==1): #αγνωούμε τις 2 πρώτες γραμμές για να κρατήσουμε μόνο την Βάση γνώσης
+            if (i == 1):
+                characters = line
+            i+=1
             continue
         line = line.strip('\n')
+        while (len(line)< l):
+            line+=' '
         kb.append(line)
     kbfile.close()
-    return kb
+    return kb,characters
 
 def retKBcharacteristics():
     """Επιστρέφει τα χαρακτηριστικά της βάσης δεδομένων
@@ -59,31 +67,36 @@ def retKBcharacteristics():
     kbfile = open('knowledgeBase.txt', 'r')
     characteristics = kbfile.readline()
     kbfile.close()
-    c, l, p = characteristics.split(',')
+    p, c, l = characteristics.split(',')
 
-    return int(c), int(l), int(p)
-
-
+    return int(p), int(c), int(l)
 
 
-def createKnowledgeBase(c=20, l=5, p=10):
+
+
+def createKnowledgeBase(p, c, l):
     """ Φτιάχνει και επιστρέφει την βάση γνώσης με c πληθος πρωτάσεων
             κάθε πρόταση μήκους 1 εως l
             και πληθος p διαθέσημα λεκτικά για κάθε πρόταση (κάθε προταση εχει διαφορετικά λεκτικά)
         Επίσης η βάση γνώσης γράφεται στο αρχειο knowladgeBase.txt"""
+
     kb = []
-    for i in range(c):
+    availableCharacters = "abc"
+    for i in range(c+1):
         sentance = ''
         while sentance in kb:
-            sentance = retSentance(l, p)
+            sentance = retSentance(l, p,availableCharacters)
         kb.append(sentance)
 
     # Εγραφή της Βάσης σε αρχείο
     knowledgeBaseFile = open('knowledgeBase.txt', 'w')
-    knowledgeBaseFile.write(str(c)+','+str(l)+','+str(p))
+    knowledgeBaseFile.write(str(p) + ',' + str(c) + ',' + str(l))
+    knowledgeBaseFile.write('\n')
+    knowledgeBaseFile.write(availableCharacters)
     for i in kb:
         knowledgeBaseFile.write(i)
         knowledgeBaseFile.write('\n')
 
     knowledgeBaseFile.close()
     return kb
+#createKnowledgeBase(3, 5, 3)
--- main.py
+++ main.py
@@ -1,18 +1,143 @@
 import createKnowledgeBase as ckb
-import sentenceClass
+import random
 
-def gsat(a,maxTries, maxFlips):
-    pass
+def data_structure(chars,kb,binaryvalues):
+    p, c, l = ckb.retKBcharacteristics()
+    lst=[]
+    counter=0
+    for i in range(c):
+        k = 0
+        while (k <= p): # ελέγχουμε αν το κάθε στοιχείο του chars είναι ίσο με το
+            if chars[k] in kb[i]:  # εαν το στοιχείο j του chars είναι ίσο με το kb πήγαινε
+                if binaryvalues[k] == True:  # εάν είναι true τότε όλη η πρόταση θα είναι true οπότε δεν χρειάζεται περεταίρο αναζήτηση προχωράμε στην επόμενη
+                    lst.append(bool(1))
+                    break
+            elif chars[k].swapcase() in kb[i]:  # εαν το κεφαλαίο στοιχείο του chars είναι ίσο με το kb
+                if binaryvalues[k] == False:
+                    lst.append(bool(1))
+                    break
+            if (k == p):
+                lst.append(bool(0))
+            k+=1
+    for i in lst:
+        if i == False:
+            counter+=1
+    lst.append(counter)
+    return lst
+
+def flip(values,i):
+    binaryvalues=values
+    if binaryvalues[i] == True:
+        binaryvalues[i] = False
+    elif binaryvalues[i] == False:
+        binaryvalues[i] = True
+
+
+def gsat(kb,maxTries,maxFlips):
+    kb, characters = ckb.readKnowledgeBase()
+    p, c, l = ckb.retKBcharacteristics()
+    binaryvalues=[]#μεταβλητή για να κρατάει boolean τιμές στις θέσεις τον λεκτικών
+    mincost=l#μεταβλητή για να κρατάει το μικρότερο κόστος των κινήσεων
+    breakcondition=0
+    data=[]
+
+    for i in range(maxTries): #max προσπάθειες
+        if breakcondition == 1:
+            break
+        for i in range(p):#αναθέτουμε random bool τιμές
+            binaryvalues.append(bool(random.getrandbits(1)))
+        print(binaryvalues)
+
+        for i in range(maxFlips):
+            tmpointer=[] #μεταβλητή που έχει δείκτη/ες από για τις αλλαγές με το μικρότερο κόστος
+            tmplist=[] #λίστα για να κρατάει τα κόστη των κινήσεων
+            data.append(data_structure(characters,kb,binaryvalues))
+            for i in range(p):#συνάρτηση για να βρει τα μέγιστο κόστος
+                values=binaryvalues.copy()# το copy χρειάζεται γιατί χωρίς αυτό ο πίνακας values παίρνει τις τιμές του binaryvalues και λειτουργεί σαν δείκτης για τον binary
+                flip(values,i)
+                tmplist.append(data_structure(characters,kb,values)[-1])#αποθηκεύεται το κόστος της κάθε αλλαγής μέσα στην λίστα
+            print("λίστα με τα κόστη",tmplist)
+            for i in tmplist: #συνάρτηση για να βρίσκει το μικρότερο κόστος
+                if(i < mincost):
+                    mincost = i
+            print("κοστος",mincost)
+
+            for i in range(len(tmplist)):#λίστα για να κρατάει τους δείκτες των τιμών όπου η αλλαγή τους έχει το μικρότερο κόστος
+                if(tmplist[i] == mincost):
+                    tmpointer.append(i)
+            print("δείκτες kai xaraktires",tmpointer)
+
+            if(len(tmpointer)>1):
+                flip(binaryvalues,tmpointer[random.randint(0,len(tmpointer)-1)])
+            else:
+                print("tmpointer",tmpointer[0])
+                flip(binaryvalues,tmpointer[0])
+            print(binaryvalues)
+            if mincost == 0:
+                data.append(data_structure(characters, kb, binaryvalues))
+                print(data)
+                print("Congrats every sentence in the KB is True ")
+                breakcondition=1
+                break
+            print("--------")
+
+
+def sentenceFullResolusion(sentance1, sentance2):
+    """Υλοποιει την πληρης ανάλυση, αφαιρει τα λεκτικα που εμφανίζονται αντεστραμενα στην αλλη προταση
+    parameters:
+        sentance1 (string): Η μία πρόταση
+        sentance2 (string): Η άλλη πρόταση
+
+    returns:
+        result (string): Το αποτέλεσμα της ανάλυσης
+    """
+    result = sentance1+sentance2
+    for i in result:
+        if i.swapcase() in result:
+            result = result.replace(i, '')
+            result = result.replace(i.swapcase(), '')
+
+    return result
+
+def resolution(kb, newCharacter=''):
+    tmpKB = kb.copy()
+    newCharacter = newCharacter.swapcase()  # Ελέγχουμε για το not απο αυτο που εισάγχθηκε
+    newSentences = []
+    # Ελεχγο για το ποιες προτάσεις μπορεί να γίνει ανάλυση υλοποίηση της ανάλυσης
+
+    while (1):
+        availableResolutions = []
+        for i in range(len(tmpKB)):
+            if (newCharacter.swapcase() in tmpKB[i]):
+                availableResolutions.append(i)  # σε ποιες προτάσεις μπορουν να γίνει resolution
+        if (len(availableResolutions) == 0):
+            # Δεν μπορούν αν γίνουν άλλες αναλύσης, δεν ξέρεις τι σημαίνει ακόμα αυτό
+            break
+        for i in availableResolutions:
+            newSentences.append(sentenceFullResolusion(tmpKB[i], newCharacter))
+        tmpKB.append(newSentences)
+        print(tmpKB)
+
+    return newSentences
+
+
+
+
 
 def main():
+    kb, characters = ckb.readKnowledgeBase()
+    #gsat(kb,10,20)
 
-    kb = ckb.readKnowledgeBase()
-    classedKB = []
-    for i in kb:
-        classedKB.append(sentenceClass.sentence(i))
+    print(resolution(kb, "b"))
 
-    print(classedKB[7].sentenceString, classedKB[8].sentenceString)
-    print(classedKB[7].sentenceFullResolusion(classedKB[8]))
+
+
+    print("Hello")
+
+
+
+
+
 
 
 
